e50a2867f6ff3da563ee6ed3f1d9e6500b6a4cf3
Marcio Katsumi Yamashita <marciokatsumiyamashita@gmail.com>
2017-03-07 11:39:19 -0300

    initial files

---
+++ README.md
@@ -0,0 +1,11 @@
+# OCN_run_script
+============
+Plugin to run script
+
+This is an alpha release, use with care, feedback & code welcome!
+
+# Menu
+There are 2 menu items under `Preferences\Package Settings\OCN_run_script`:
+
+- `Setting Prompt - User` - Open a custom setting to provide a user prompt
+- `Setting Script - User` - Open a custom commands and provides a script
---
+++ ocn_script.py
@@ -0,0 +1,152 @@
+import sublime
+import sublime_plugin
+import os
+import sys
+import subprocess
+import locale
+
+if os.name == 'nt':
+    try:
+        import _winreg
+    except (ImportError):
+        import winreg as _winreg
+    from ctypes import windll, create_unicode_buffer
+
+class NotFoundError(Exception):
+    pass
+
+if sys.version_info >= (3,):
+    installed_dir, _ = __name__.split('.')
+else:
+    installed_dir = os.path.basename(os.getcwd())
+
+def get_setting(key, default=None):
+    settings = sublime.load_settings('OCN_run_script.sublime-settings')
+    os_specific_settings = {}
+    return os_specific_settings.get(key, settings.get(key, default))
+
+DEFAULT_CONTENT = """[
+    {   // Define your custom Script here
+        // Name to show in Command Palette
+        "caption": "File: New",
+        // Command to invoke
+        "command": "new_file",
+        // script to run
+        "args": {"script": ["value"]}
+    }
+]"""
+DEFAULT_PROMPT = """{// Define your custom Prompt here
+        "prompt": "",
+}"""
+
+class TerminalSelector():
+    default = None
+
+    @staticmethod
+    def get():
+        package_dir = os.path.join(sublime.packages_path(), installed_dir)
+        terminal = get_setting('prompt')
+        if terminal:
+            dir, executable = os.path.split(terminal)
+            return terminal
+
+        default = None
+
+        if os.name == 'nt':
+            default = os.environ['SYSTEMROOT'] + '\\System32\\cmd.exe'
+        TerminalSelector.default = default
+        return default
+
+class TerminalCommand():
+    def get_path(self, paths):
+        if paths:
+            return paths[0]
+        # DEV: On ST3, there is always an active view.
+        #   Be sure to check that it's a file with a path (not temporary view)
+        elif self.window.active_view() and self.window.active_view().file_name():
+            return self.window.active_view().file_name()
+        elif self.window.folders():
+            return self.window.folders()[0]
+        else:
+            sublime.error_message('Terminal: No place to open terminal to')
+            return False
+
+    def run_script(self, dir_, parameters):
+        try:
+            for k, v in enumerate(parameters):
+                parameters[k] = v.replace('%CWD%', dir_)
+            args = [TerminalSelector.get()]
+            args.extend(parameters)
+            encoding = locale.getpreferredencoding(do_setlocale=True)
+            if sys.version_info >= (3,):
+                cwd = dir_
+            else:
+                cwd = dir_.encode(encoding)
+            # Copy over environment settings onto parent environment
+            env_setting = get_setting('env', {})
+            env = os.environ.copy()
+            for k in env_setting:
+                if env_setting[k] is None:
+                    env.pop(k, None)
+                else:
+                    env[k] = env_setting[k]
+
+            # Normalize environment settings for ST2
+            # https://github.com/wbond/sublime_terminal/issues/154
+            # http://stackoverflow.com/a/4987414
+            for k in env:
+                if not isinstance(env[k], str):
+                    if isinstance(env[k], unicode):
+                        env[k] = env[k].encode('utf8')
+                    else:
+                        print('Unsupported environment variable type. Expected "str" or "unicode"', env[k])
+            # Run our process
+            subprocess.Popen(args, cwd=cwd, env=env)
+
+        except (OSError) as exception:
+            print(str(exception))
+            sublime.error_message('Terminal: The terminal ' +
+                TerminalSelector.get() + ' was not found')
+        except (Exception) as exception:
+            sublime.error_message('Terminal: ' + str(exception))
+
+
+class OpenScriptCommand(sublime_plugin.WindowCommand, TerminalCommand):
+    def run(self, paths=[], script=None):
+        path = self.get_path(paths)
+
+        if not path:
+            return
+
+        if os.path.isfile(path):
+            path = os.path.dirname(path)
+
+        self.run_script(path, script)
+
+class EditPromptUserCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        """Open `Packages/User/OCN_run_script.sublime-settings` for custom definitions"""
+        filepath = os.path.join(sublime.packages_path(), 'User\OCN_run_script.sublime-settings')
+        print(filepath)
+        if not os.path.isfile(filepath):
+            with open(filepath, 'w') as f:
+                f.write(DEFAULT_PROMPT)
+
+        # Open the commands file
+        self.window.run_command('open_file', {
+            'file': filepath,
+        })
+
+class EditScriptUserCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        """Open `Packages/User/Default.sublime-commands` for custom definitions"""
+        filepath = os.path.join(sublime.packages_path(), 'User\OCN_run_script.sublime-commands')
+        print(filepath)
+        if not os.path.isfile(filepath):
+            with open(filepath, 'w') as f:
+                f.write(DEFAULT_CONTENT)
+
+        # Open the commands file
+        self.window.run_command('open_file', {
+            'file': filepath,
+        })
